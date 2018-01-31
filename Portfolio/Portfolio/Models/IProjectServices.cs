c7389316727b1727e2f4cba2a1d976af75cebdd0
QueensKid <andres.ibarra0716@gmail.com>
2018-01-30 22:48:51 -0800

    Completed most of the basic scafflding from previous two days.

---
+++ Portfolio/Portfolio/Models/IProjectServices.cs
@@ -0,0 +1,24 @@
+﻿using Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio
+{
+    interface IProjectServices 
+    {
+        Task DeleteAsync(int id);
+
+        Project Find(int id);
+
+        Task<Project> FindAsync();
+
+        IQueryable<Project> GetAll(int? count = null, int? page = null );
+
+        Task<Project[]> GetAllAsync(int? count = null, int? page = null);
+
+        Task SaveAsync(Project projects);
+
+    }
+}
---
+++ Portfolio/Portfolio/Models/PortfolioDbContextSeed.cs
@@ -0,0 +1,67 @@
+﻿using Portfolio.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class PortfolioDbContextSeed
+    {
+
+        static object synchblock = new object();
+        static volatile bool seeded = false;
+        public static void EnsureSeedData(this PortfolioDbContext context)
+        {
+            if (!seeded && context.Projects.Count() == 0)
+            {
+                lock (synchblock)
+                {
+                    if (!seeded)
+                    {
+                        var projects = GenerateProjects();
+                        context.Projects.AddRange(projects);
+                        context.SaveChanges();
+                        seeded = true;
+                    }
+                }
+
+            }
+        }
+
+        public static Project[] GenerateProjects()
+        {
+            return new Project[]
+            {
+                new Project
+            {
+                Name = "add info here",
+
+                Title = "add infor here",
+
+                Description = string.Join(Environment.NewLine, new List<string> {
+                        "add info here",
+                        "add infor here"
+                    }),
+
+                    ImageUrl = "place image path here"
+                },
+
+                new Project
+                {
+                    Name = "add info here",
+
+                    Title = "add info here",
+
+                    Description = string.Join(Environment.NewLine, new List<string>
+                    {
+                        "add info here",
+                        "add info here"
+                    }),
+
+                    ImageUrl = "place image path here"
+                }
+            };
+        }
+    }
+}
