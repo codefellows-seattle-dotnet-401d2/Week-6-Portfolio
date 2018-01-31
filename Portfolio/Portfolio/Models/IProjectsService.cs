99c6d1bc1313b22e919d934d003fdc399be23150
jeffxmartinez <jeffxmartinez@gmail.com>
2018-01-30 18:34:19 -0800

    Added bootstrap

---
+++ Portfolio/Portfolio/Models/IProjectsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    interface IRestaurantService
+    {
+        Task DeleteAsync(int id);
+
+        Projects Find(int id);
+
+        Task<Projects> FindAsync();
+
+        IQueryable<Projects> GetAll(int? count = null, int? page = null );
+
+        Task<Projects[]> GetAllAsync(int? count = null, int? page = null);
+
+        Task SaveAsync(Projects projects);
+    }
+}
---
+++ Portfolio/Portfolio/Models/Projects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Environment;
+
+namespace Portfolio.Models
+{
+    public class Projects
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Technologies { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+}
