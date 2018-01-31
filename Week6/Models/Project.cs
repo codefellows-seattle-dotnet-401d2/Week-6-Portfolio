23077ffc1f8de98e35f1f2536d3119d35a0e7586
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-30 22:41:18 -0800

    Wrote this in a seperate project so I'm submitting it properly here.

---
+++ Week6/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week6.Models
+{
+    public class Project
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public List<string> Images { get; set;  }
+        public string Description { get; set; }
+        public string Languages { get; set; }
+        public string Repository { get; set; }
+
+
+    }
+}
---
+++ Week6/Models/ProjectService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week6.Models
+{
+    public class ProjectService : IProjectService
+    {
+        public ProjectDbContext _context { get; set; }
+
+        public ProjectService(ProjectDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostAsync(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PutAsync(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
