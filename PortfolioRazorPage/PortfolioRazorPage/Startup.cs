df2df73599e691e7fdf58d46f07c79146d7d7a8d
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-30 22:43:33 -0800

    DbContext stuff

--- PortfolioRazorPage/PortfolioRazorPage/Startup.cs
+++ PortfolioRazorPage/PortfolioRazorPage/Startup.cs
@@ -5,17 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PortfolioRazorPage.Data;
 
 namespace PortfolioRazorPage
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ProjectDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
