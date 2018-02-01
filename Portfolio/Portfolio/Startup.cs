f19b6dcaf7cd576a2a832611400f7473f3daa991
Luay <luayghazwan@yahoo.com>
2018-02-01 09:28:53 -0800

    startup updated

--- Portfolio/Portfolio/Startup.cs
+++ Portfolio/Portfolio/Startup.cs
@@ -15,6 +15,7 @@ namespace Portfolio
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,9 @@ namespace Portfolio
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
