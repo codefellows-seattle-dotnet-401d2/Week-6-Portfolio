6ef493df4406e7e9dffd5fa98de1d61d99deccc9
Luay <luayghazwan@yahoo.com>
2018-02-01 11:14:21 -0800

    Added Project DbContext

---
+++ Portfolio/Portfolio/Models/ProjectDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio.Models
+{
+    public class ProjectDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
--- Portfolio/Portfolio/Models/ProjectService.cs
+++ Portfolio/Portfolio/Models/ProjectService.cs
@@ -7,6 +7,11 @@ namespace Portfolio.Models
 {
     public class ProjectService : IProjectService
     {
+        private readonly ProjectDbContext _context;
+        public ProjectService(ProjectDbContext context)
+        {
+            _context = context;
+        }
         public Task DeleteAsync(int id)
         {
             throw new NotImplementedException();
