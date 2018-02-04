beaf9ae294fe4be81724696466478263aaf841fe
Ariel Pedraza <pedrazaa2@gmail.com>
2018-02-03 17:23:37 -0800

    created DbContext

---
+++ Portfolio/Portfolio/Data/ProjectDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Data
+{
+    public class ProjectDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
