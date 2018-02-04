1f8f838b00e5cdfa4f9d98e51e24d4831e0fd86d
Ariel Pedraza <pedrazaa2@gmail.com>
2018-02-03 17:18:10 -0800

    created interface and service class that implements it.

---
+++ Portfolio/Portfolio/Models/IProjectService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    interface IProjectService
+    {
+        Task DeleteAsync(int id);
+        Project Find(int id);
+        Task<Project> FindAsync();
+        IQueryable<Project> GetAll(int? count = null, int? page = null);
+        Task<Project[]> GetAllAsync(int? count = null, int? page = null);
+        Task SaveAsync(Project project);
+    }
+}
--- Portfolio/Portfolio/Models/Project.cs
+++ Portfolio/Portfolio/Models/Project.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Environment;
 
 namespace Portfolio.Models
 {
@@ -14,5 +15,9 @@ namespace Portfolio.Models
         public string ImageUrl { get; set; }
         public byte[] Image { get; set; }
         public string ImageContentType { get; set; }
+        public IEnumerable<string> SkillItemList
+        {
+            get { return (SkillItems ?? String.Empty).Split(NewLine); }
+        }
     }
 }
