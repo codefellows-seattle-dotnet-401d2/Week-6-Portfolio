ffc2ec81dd9332251351e7eff32360acb1d3ac85
Ariel Pedraza <pedrazaa2@gmail.com>
2018-02-03 16:36:33 -0800

    created project model

---
+++ Portfolio/Portfolio/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SkillItems { get; set; }
+        public string ImageUrl { get; set; }
+        public byte[] Image { get; set; }
+        public string ImageContentType { get; set; }
+    }
+}
