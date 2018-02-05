c7f667a710da0e75b01afc5a6a532954af59b0b6
Luay <luayghazwan@yahoo.com>
2018-02-05 01:14:02 -0800

    Add edit Project Razor page created with Get and Post logic

---
+++ Portfolio/Portfolio/Pages/Admin/AddEditProject.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Models;
+
+namespace Portfolio.Pages.Admin
+{
+    public class AddEditProjectModel : PageModel
+    {
+        [FromRoute]
+        public int? Id { get; set; }
+
+        [BindProperty]
+        public Project Project { get; set; }
+
+        private readonly IProjectService ProjectService;
+
+        public AddEditProjectModel(IProjectService ps)
+        {
+            ProjectService = ps;
+        }
+
+        public async Task OnGet()
+        {
+            Project = await ProjectService.FindAsync(Id.GetValueOrDefault()) ?? new Project();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var project = await ProjectService.FindAsync(Id.GetValueOrDefault()) ?? new Project();
+            project.Name = Project.Name;
+            project.Description = Project.Description;
+            project.Type = Project.Type;
+            project.DateCompleted = Project.DateCompleted;
+            project.Technologies = Project.Technologies;
+
+            await ProjectService.SaveAsync(project);
+
+            return RedirectToPage("/project", new { Id = project.Id });
+        }
+
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await ProjectService.DeleteAsync(Id.GetValueOrDefault());
+            return RedirectToPage("/index");
+        }
+    }
+}
