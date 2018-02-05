5621fecbdd3868b100a4f0b07a5f1014833ba2a2
Ariel Pedraza <pedrazaa2@gmail.com>
2018-02-04 16:45:08 -0800

    added edit project razor page

---
+++ Portfolio/Portfolio/Pages/Admin/AddEditProject.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Models;
+
+namespace Portfolio.Pages.Admin
+{
+    public class AddEditProjectModel : PageModel
+    {
+        [FromRoute]
+        public int? Id { get; set; }
+
+        [BindProperty]
+        public Project Project { get; set; }
+
+        private readonly IProjectService projectService;
+
+        public AddEditProjectModel(IProjectService ps)
+        {
+            projectService = ps;
+        }
+
+        public async Task OnGet()
+        {
+            Project = await projectService.FindAsync(Id.GetValueOrDefault()) ?? new Project();
+        }
+    }
+}
