f2bad445152d380eb12c9ed5c0301b6736929bf7
Luay <luayghazwan@yahoo.com>
2018-02-06 01:53:21 -0800

    User model with Email and Password Properties with their data annotation

---
+++ Portfolio/Portfolio/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address"), DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
