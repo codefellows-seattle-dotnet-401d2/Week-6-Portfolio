aa8cd00026bdd1b06b91efb6c17219a486f23d3f
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-10 18:29:50 -0800

    Added Login razor page.

--- Portfolio/Portfolio/Startup.cs
+++ Portfolio/Portfolio/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,10 +13,9 @@ namespace Portfolio
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie;
             services.AddMvc();
             services.AddScoped<IProjectService, ProjectService>();
         }
