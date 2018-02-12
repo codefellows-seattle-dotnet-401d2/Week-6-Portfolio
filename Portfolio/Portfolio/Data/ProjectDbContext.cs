30ad39f533b9bb2376452d271dde99e54a5a9abd
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-12 10:45:27 -0800

    Attempting to do migration, asking for an IDesignTimeDbContext implementation

--- Portfolio/Portfolio/Data/ProjectDbContext.cs
+++ Portfolio/Portfolio/Data/ProjectDbContext.cs
@@ -1,19 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Portfolio.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Portfolio.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Portfolio.Data
 {
     public class ProjectDbContext : DbContext
     {
-        public DbSet<Project> Projects { get; set; }
-
         public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
         {
             this.EnsureSeedData();
         }
+
+        public DbSet<Project> Projects { get; set; }
     }
 }
