ad28802e66abfd2baa0ffea43a8bee074e9c8994
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-02-12 11:47:32 -0800

    got migration/update-database to work by implementing Design-time DbContext interface

---
+++ Portfolio/Portfolio/Data/ProjectContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Portfolio.Data
+{
+    public class ProjectContextFactory : IDesignTimeDbContextFactory<ProjectDbContext>
+    {
+        public ProjectDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ProjectDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=PortfolioDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+            // optionsBuilder.UseSqlite("Data Source=blog.db");
+
+            return new ProjectDbContext(optionsBuilder.Options);
+        }
+    }
+}
